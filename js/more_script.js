afd2029af06cd7ca03da0f06360c0cf91ca8ddf0
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-04-04 01:55:43 +0000

    done both pages. do css

---
+++ js/more_script.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Madeleine Forgeron All rights reserved
+//
+// Created by: Madeleine Forgeron
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  const cm = parseFloat(document.getElementById('cm-to-feet').value)
+
+  // process
+  const feet = (cm / 30.48)
+
+  // output
+  document.getElementById('feet').innerHTML = 'Your shoe size in feet is: ' + feet.toFixed(2) + ' feet '
+}
+
+// button
+function firstPage() {
+  window.location='https://assign-02-html-calculations.madeleineforger.repl.co';
+}
